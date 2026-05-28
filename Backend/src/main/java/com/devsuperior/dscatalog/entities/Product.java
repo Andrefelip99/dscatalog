e563791ea8c45a8480c3a4686be9dceceb9adad9
andre felipe <andre.felipe106@icloud.com>
2026-05-28 16:33:10 -0300

    category resource

---
+++ Backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dscatalog.entities;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "tb_product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+  
+}
