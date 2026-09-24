f41e29eccd6f3a47ea3d4d603d21ae4a9e52bdf4
andre felipe <andre.felipe106@icloud.com>
2026-09-23 21:53:24 -0300

    implements Security

---
+++ Backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -0,0 +1,56 @@
+package com.devsuperior.dscatalog.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.devsuperior.dscatalog.entities.User;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    Set<RoleDTO> roles = new HashSet<>();
+
+    public UserDTO(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        firstName = entity.getFirstName();
+        lastName = entity.getLastName();
+        email = entity.getEmail();
+        entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+}
---
+++ Backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dscatalog.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInsertDTO extends UserDTO {
+    private static final long serialVersionUID = 1L;
+
+    private String password;
+
+    public  UserInsertDTO(){
+        super();
+    }
+    
+
+}
